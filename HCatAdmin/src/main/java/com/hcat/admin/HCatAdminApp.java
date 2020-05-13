fe90073f60c6d5b98f7291e42ca013448416d9fc
YaoQi <168412217@qq.com>
2020-05-13 22:58:53 +0800

    initial project

---
+++ HCatAdmin/src/main/java/com/hcat/admin/HCatAdminApp.java
@@ -0,0 +1,16 @@
+package com.hcat.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YaoQi
+ * Date: 2020/5/13 22:54
+ * Description:
+ */
+@SpringBootApplication
+public class HCatAdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HCatAdminApp.class, args);
+    }
+}
---
+++ HCatCore/src/main/java/com/hcat/core/HCatCoreApp.java
@@ -0,0 +1,16 @@
+package com.hcat.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YaoQi
+ * Date: 2020/5/13 22:50
+ * Description:
+ */
+@SpringBootApplication
+public class HCatCoreApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HCatCoreApp.class, args);
+    }
+}
